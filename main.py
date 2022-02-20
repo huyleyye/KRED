32428f37037570eb987b90edb8d2b2e9252fe32f
Keven Li <keven0826@icloud.com>
2022-02-20 16:09:05 +0800

    error fix

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
                       help='config file path (default: None)')
     parser.add_argument('-r', '--resume', default=None, type=str,
                       help='path to latest checkpoint (default: None)')
-    parser.add_argument('-d', '--device', default=all, type=str,
+    parser.add_argument('-d', '--device', default=None, type=str,
                       help='indices of GPUs to enable (default: all)')
 
     config = ConfigParser.from_args(parser)
